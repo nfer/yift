8f65fdf6675d6a3d5e7d9e0502488c9cabb2270b
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-11-01 09:20:11 +0800

    update file name and structure

---
+++ src/components/home/HomeContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-container">
+    <header-container></header-container>
+  </div>
+</template>
+
+<script>
+import HeaderContainer from '@/components/header/HeaderContainer.vue'
+
+export default {
+  name: 'HomeContainer',
+  components: {
+    'header-container': HeaderContainer
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
